241b9b7e26fb3bdebc39606955e63a7216cefd72
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-03-09 22:30:19 +0200

    print formatting

--- debug.c
+++ debug.c
@@ -15,7 +15,7 @@ int disasembleInstruction(Chunk *chunk, int offset) {
   if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
     printf("  |");
   } else {
-    printf("%4d", chunk->lines[offset]);
+    printf("%4d ", chunk->lines[offset]);
   }
   uint8_t instruction = chunk->code[offset];
 
@@ -32,7 +32,7 @@ int disasembleInstruction(Chunk *chunk, int offset) {
 
 int constantInstruction(const char *name, Chunk *chunk, int offset) {
   uint8_t constant = chunk->code[offset + 1];
-  printf("%-16s %4d ", name, constant);
+  printf("%-16s %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
   printf("'\n");
   return offset + 2;
