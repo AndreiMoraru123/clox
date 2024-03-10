d66eafaa9f004b51a759f597eb61cebbde0ff3ea
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-03-10 20:31:36 +0200

    manually generated bytecode instruction sequences

--- main.c
+++ main.c
@@ -9,27 +9,117 @@ int main(int argc, char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  {
+    /* 1 * 2 + 3 */
+    int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
 
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
 
-  writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_MULTIPLY, 1);
 
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
 
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
+    writeChunk(&chunk, OP_ADD, 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeChunk(&chunk);
+  }
 
-  writeChunk(&chunk, OP_RETURN, 123);
-  disassembleChunk(&chunk, "test chunk");
-  interpret(&chunk);
+  {
+    /* 1 + 2 * 3 */
+    int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeChunk(&chunk);
+  }
+
+  {
+    /* 3 - 2 - 1 */
+    int constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeChunk(&chunk);
+  }
+
+  {
+    /* 1 + 2 * 3 - 4 / -5 */
+
+    // 2 * 3
+    int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    // 1 + (result of 2 * 3)
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+    // 4 / -5 (negating 5 first)
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_NEGATE, 1);
+    writeChunk(&chunk, OP_DIVIDE, 1);
+
+    // (result of previous operations) - (result of 4 / -5)
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+    writeChunk(&chunk, OP_RETURN, 1);
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeChunk(&chunk);
+  }
+
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
