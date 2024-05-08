d99e470009c147135246e5dd9e0ff6d504886d40
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-05-08 22:37:40 +0300

    adds arity checking for native fns

--- vm.c
+++ vm.c
@@ -43,9 +43,9 @@ static void runtimeError(const char *format, ...) {
   resetStack();
 }
 
-static void defineNative(const char *name, NativeFn function) {
+static void defineNative(const char *name, NativeFn function, int arity) {
   push(OBJ_VAL(copyString(name, (int)strlen(name))));
-  push(OBJ_VAL(newNative(function)));
+  push(OBJ_VAL(newNative(function, arity)));
   tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
   pop();
   pop();
@@ -56,7 +56,7 @@ void initVM() {
   vm.objects = NULL;
   initTable(&vm.globals);
   initTable(&vm.strings);
-  defineNative("clock", clockNative);
+  defineNative("clock", clockNative, 0);
 }
 
 void freeVM() {
@@ -100,8 +100,13 @@ static bool callValue(Value callee, int argCount) {
     case OBJ_FUNCTION:
       return call(AS_FUNCTION(callee), argCount);
     case OBJ_NATIVE: {
-      NativeFn native = AS_NATIVE(callee);
-      Value result = native(argCount, vm.stackTop - argCount);
+      ObjNative *native = AS_NATIVE(callee);
+      if (argCount != native->arity) {
+        runtimeError("Expected %d arguments but got %d.", native->arity,
+                     argCount);
+        return false;
+      }
+      Value result = native->function(argCount, vm.stackTop - argCount);
       vm.stackTop -= argCount + 1;
       push(result);
       return true;
