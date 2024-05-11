2a1de039e79ab13be0b216eb842dfbaf6ae707e0
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-05-11 20:07:04 +0300

    closures

--- chunk.h
+++ chunk.h
@@ -20,6 +20,8 @@ typedef enum {
   OP_GET_GLOBAL,
   OP_DEFINE_GLOBAL,
   OP_SET_GLOBAL,
+  OP_GET_UPVALUE,
+  OP_SET_UPVALUE,
   OP_EQUAL,
   OP_GREATER,
   OP_LESS,
@@ -30,6 +32,8 @@ typedef enum {
   OP_NOT,
   OP_NEGATE,
   OP_PRINT,
+  OP_CLOSURE,
+  OP_CLOSE_UPVALUE,
 } OpCode;
 
 typedef struct {
