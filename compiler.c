a3a35b050030f6eadbebd045c5cd1ed9163383ed
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-05-12 17:26:44 +0300

    fixed bug: popped twice for ending scope

--- compiler.c
+++ compiler.c
@@ -223,12 +223,12 @@ static void endScope() {
 
   while (current->localCount > 0 &&
          current->locals[current->localCount - 1].depth > current->scopeDepth) {
+
     if (current->locals[current->localCount - 1].isCaptured) {
       emitByte(OP_CLOSE_UPVALUE);
     } else {
       emitByte(OP_POP);
     }
-    emitByte(OP_POP);
     current->localCount--;
   }
 }
